c6e098b90ce0d40d249a80814b2eead794a43329
summertime-fms <danyushin0107@gmail.com>
2021-08-02 07:37:06 +0300

    добавляет запросы в сценарий

--- index.php
+++ index.php
@@ -1,27 +1,39 @@
 <?php
+$con = mysqli_connect('127.0.0.1', 'root', '', 'yeticave');
+mysqli_set_charset($con, 'utf-8');
+
+if (!$con) {
+    print('Ошибка подключения: ' . mysqli_connect_error());
+} else {
+    $sql_cats = 'SELECT code, category FROM categories';
+    $sql_lots = 'SELECT name, description, start_cost, img_link, termination_date, category_id FROM lots';
+    $result_cats = mysqli_query($con, $sql_cats);
+    $result_lots = mysqli_query($con, $sql_lots);
+
+    if (!$result_cats || !$result_lots) {
+        $error = mysqli_error($con);
+        print('Ошибка SQL: ' . $error);
+    } else {
+        $cats = mysqli_fetch_all($result_cats, MYSQLI_ASSOC);
+        $lots = mysqli_fetch_all($result_lots, MYSQLI_ASSOC);
+    }
+}
+
 $is_auth =  rand(0, 1);
 $user_name = 'Михаил Данюшин';
 $title = 'YetiCave || Главная';
-$cats = ['boards' => 'Доски и лыжи',
-         'attachment' => 'Крепления',
-         'boots' => 'Ботинки',
-         'clothing' => 'Одежда',
-         'tools' => 'Инструменты',
-         'other' => 'Разное'];
-
-$items = [
-            ['name' => '2014 Rossignol District Snowboard', 'cat' => $cats['boards'], 'cost' => 10999, 'img_url' => 'img/lot-1.jpg', 'date' => '2021-08-01'],
-            ['name' => 'DC Ply Mens 2016/2017 Snowboard', 'cat' => $cats['boards'], 'cost' => 159999, 'img_url' => 'img/lot-2.jpg', 'date' => '2021-07-25'],
-            ['name' => 'Крепления Union Contact Pro 2015 года размер L/XL', 'cat' => $cats['attachment'], 'cost' => 8000, 'img_url' => 'img/lot-3.jpg', 'date' => '2021-08-02'],
-            ['name' => 'Ботинки для сноуборда DC Mutiny Charocal', 'cat' => $cats['boots'], 'cost' => 10999, 'img_url' => 'img/lot-4.jpg', 'date' => '2021-08-10'],
-            ['name' => 'Куртка для сноуборда DC Mutiny Charocal', 'cat' => $cats['clothing'], 'cost' => 7500, 'img_url' => 'img/lot-5.jpg', 'date' => '2021-07-19'],
-            ['name' => 'Маска Oakley Canopy', 'cat' => $cats['other'], 'cost' => 5400, 'img_url'=>'img/lot-6.jpg', 'date' => '2021-08-01']
-        ];
+// $items = [
+//     ['name' => '2014 Rossignol District Snowboard', 'cat' => $cats['boards'], 'cost' => 10999, 'img_url' => 'img/lot-1.jpg', 'date' => '2021-08-01'],
+//     ['name' => 'DC Ply Mens 2016/2017 Snowboard', 'cat' => $cats['boards'], 'cost' => 159999, 'img_url' => 'img/lot-2.jpg', 'date' => '2021-07-25'],
+//     ['name' => 'Крепления Union Contact Pro 2015 года размер L/XL', 'cat' => $cats['attachment'], 'cost' => 8000, 'img_url' => 'img/lot-3.jpg', 'date' => '2021-08-02'],
+//     ['name' => 'Ботинки для сноуборда DC Mutiny Charocal', 'cat' => $cats['boots'], 'cost' => 10999, 'img_url' => 'img/lot-4.jpg', 'date' => '2021-08-10'],
+//     ['name' => 'Куртка для сноуборда DC Mutiny Charocal', 'cat' => $cats['clothing'], 'cost' => 7500, 'img_url' => 'img/lot-5.jpg', 'date' => '2021-07-19'],
+//     ['name' => 'Маска Oakley Canopy', 'cat' => $cats['other'], 'cost' => 5400, 'img_url' => 'img/lot-6.jpg', 'date' => '2021-08-01']
+// ];
 
 require_once('helpers.php');
 require_once('functions.php');
 
-$content = include_template('main.php', ['cats' => $cats, 'items' => $items]);
+$content = include_template('main.php', ['cats' => $cats, 'lots' => $lots]);
 $layout_content = include_template('layout.php', ['content' => $content, 'title' => $title, 'user_name' => $user_name, 'cats' => $cats]);
 print($layout_content);
-?>
--- templates/main.php
+++ templates/main.php
@@ -2,11 +2,12 @@
     <h2 class="promo__title">Нужен стафф для катки?</h2>
     <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
     <ul class="promo__list">
-        <?php foreach ($cats as $modifier => $category_name): ?>
-            <li class="promo__item promo__item--<?= $modifier; ?>">
-                <a class="promo__link" href="pages/all-lots.html"><?= htmlspecialchars($category_name); ?></a>
+        <?php foreach ($cats as $cat) : ?>
+            <li class="promo__item promo__item--<?= $cat['code']; ?>">
+                <a class="promo__link" href="pages/all-lots.html"><?= htmlspecialchars($cat['category']); ?></a>
             </li>
-        <?php endforeach; ?><!--заполните этот список из массива категорий-->
+        <?php endforeach; ?>
+        <!--заполните этот список из массива категорий-->
     </ul>
 </section>
 <section class="lots">
@@ -14,26 +15,26 @@
         <h2>Открытые лоты</h2>
     </div>
     <ul class="lots__list">
-        <?php foreach ($items as $item): ?>
+        <?php foreach ($lots as $lot) : ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?= htmlspecialchars($item['img_url']); ?>" width="350" height="260" alt="<?= htmlspecialchars($item['name']); ?>">
+                    <img src="<?= htmlspecialchars($lot['img_link']); ?>" width="350" height="260" alt="<?= htmlspecialchars($lot['name']); ?>">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?= htmlspecialchars($item['cat']); ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= htmlspecialchars($item['name']); ?></a></h3>
+                    <span class="lot__category"><?= htmlspecialchars($lot['category_id']); ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= htmlspecialchars($lot['name']); ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?= htmlspecialchars(get_cost($item['cost'])); ?></span>
+                            <span class="lot__cost"><?= htmlspecialchars(get_cost($lot['start_cost'])); ?></span>
                         </div>
-                        <? if (get_time_in_hours($item['date'])['hours'] <= 1) : ?> {
+                        <? if (get_time_in_hours($lot['termination_date'])['hours'] <= 1) : ?>
                             <div class="lot__timer timer timer--finishing">
-                                <?= implode(': ', get_time_in_hours($item['date'])); ?>
+                                <?= implode(': ', get_time_in_hours($lot['termination_date'])); ?>
                             </div>
-                        <?php else: ?>
+                        <?php else : ?>
                             <div class="lot__timer timer">
-                                <?= implode(': ', get_time_in_hours($item['date'])); ?>
+                                <?= implode(': ', get_time_in_hours($lot['termination_date'])); ?>
                             </div>
                         <?php endif ?>
                     </div>
