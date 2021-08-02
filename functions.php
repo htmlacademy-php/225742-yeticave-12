30b617158fd623bc8536e4aa47ae067313fd0c70
summertime-fms <danyushin0107@gmail.com>
2021-08-02 19:26:41 +0300

    выносит запрос в отдельную функцию

--- functions.php
+++ functions.php
@@ -1,12 +1,14 @@
 <?php
+
 /**
  * Форматирует цену лота в карточке
  * @param int Цена лота в числовом значении
  * @return string Итоговый формат цены в виде строки
  */
-function get_cost($number) {
-        $cost = number_format($number, 0, '', ' ');
-        return (string)$cost . ' ₽';
+function get_cost($number)
+{
+    $cost = number_format($number, 0, '', ' ');
+    return (string)$cost . ' ₽';
 }
 
 /**
@@ -14,10 +16,28 @@ function get_cost($number) {
  * @param string Дата истечения лота
  * @return array Массив, первый ключ которого содержит часы, а второй минуты, оставшиеся до истечения лота
  */
-function get_time_in_hours($date) {
+function get_time_in_hours($date)
+{
     $timestamp = strtotime($date) / 60;
     $currentTimestamp = floor(time() / 60);
-    $lotTime = ['hours' => floor(($timestamp - $currentTimestamp) / 60), 'minutes' => ($timestamp - $currentTimestamp) % 60 ];
+    $lotTime = ['hours' => floor(($timestamp - $currentTimestamp) / 60), 'minutes' => ($timestamp - $currentTimestamp) % 60];
     return $lotTime;
 }
-?>
+
+/**
+ * Запрашивает данные из БД и возвращает их в виде массива
+ * @param boolean Ресурс соединения
+ * @param string Строка-запрос в БД
+ * @return array Массив с полученными данными
+ */
+function get_data($con, $query)
+{
+    $result = mysqli_query($con, $query);
+
+    if (!$result) {
+        $error = mysqli_error($con);
+        print('Ошибка SQL: ' . $error);
+    } else {
+        return $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    }
+};
